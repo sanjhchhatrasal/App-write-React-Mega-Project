a10b55e77450937e4e7db5bdd9804b2a8c7ec890
sanjh chhatrasal <sanjhchhatrasal@gmail.com>
2024-04-17 14:01:29 +0530

    git-ignore-env

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+
+  return (
+    <>
+    <h1>A BLOG WITH APPWRITE</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/Appwrite/Auth.js
@@ -0,0 +1,31 @@
+import config from "../Config/Config";
+
+
+ export class AuthService {
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+            this.account = new Account(this.client)
+    }
+
+    async createAccount({email, password, name}){
+        try {
+          const userAccount =  await this.account.create((ID.unique()), email, password, name)
+          if (userAccount) {
+            return userAccount;
+          }else{
+            return null;
+          }
+        } catch (error) {
+            throw error;
+        }
+    }
+ }
+
+ const authService = new AuthService();
+
+ export default authService
